3f1d8d74bd7e83bfc53b60c1bce4e043d4ff686c
Jesse Shawl <jesse@jshawl.com>
2015-03-11 12:33:48 -0700

    initial commit

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'json'
+
+get '/' do
+  File.read('index.html')
+end
+
+get '/favorites' do
+  response.headers['Content-Type'] = 'application/json'
+  File.read('data.json')
+end
+
+post '/favorites' do
+  file = JSON.parse(File.read('data.json'))
+  unless params[:name] && params[:oid]
+    return "Invalid Request"
+  end
+  movie = { name: params[:name], oid: params[:oid] }
+  file << movie
+  File.write('data.json',JSON.pretty_generate(file))
+  movie.to_json
+end
